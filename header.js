3bb524fe60f454adb88adad803578f86816052e1
Sarah Österhed <sarah.osterhed@gmail.com>
2024-12-11 10:31:45 +0100

    Added link to cart.html in header.js and header.html

--- header.js
+++ header.js
@@ -1,10 +1,10 @@
 //Template för att länka till header. I html-filerna: <script src="header.js">
 const template = document.createElement('template');
 
-template.innerHTML=`
+template.innerHTML = `
     <header>
         <a href="index.html"><img src="icons/logo.svg" alt="Logotyp Boklusen" id="logo-boklusen"></a>
-        <img src="icons/cart-icon.svg" alt="Kundvagn" id="logo-cart">
+        <a href="cart.html"><img src="icons/cart-icon.svg" alt="Kundvagn" id="logo-cart"></a>
     </header>
 `
 document.body.appendChild(template.content);
