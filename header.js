687fd55e594babcca9cdc75b7f8380609daad6c9
Linn Tarvainen <linn.tarvainen@gmail.com>
2024-12-12 15:30:02 +0100

    BS-44 fixed positioning for header + footer, added main container for all content

--- header.js
+++ header.js
@@ -4,7 +4,7 @@ const template = document.createElement('template');
 template.innerHTML=`
     <header>
         <a href="index.html"><img src="icons/logo.svg" alt="Logotyp Boklusen" id="logo-boklusen"></a>
-        <img src="icons/cart-icon.svg" alt="Kundvagn" id="logo-cart">
+        <a href="cart.html"><img src="icons/cart-icon.svg" alt="Kundvagn" id="logo-cart"></a>
     </header>
 `
 document.body.appendChild(template.content);
