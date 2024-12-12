fcfb0a9b86455643108823b17fa631505362a354
MY PETTERSSON <alligatorvision@gmail.com>
2024-12-12 17:05:58 +0100

    BS-17 - Product Page - Adjusted JS code and CSS

--- productpage.js
+++ productpage.js
@@ -1,32 +1,44 @@
 
-// Fetch API - specific book
+// Fetch API - specific book 
 
-const apiUrl = `https://bookshop-backend-phi.vercel.app/products/`;
-
+// Fetch all data
 async function fetchBookData() {
     try {
-        const response = await fetch(`${apiUrl}${id}`); // behövde vi få hjälp att justera API:et för detta?
-        const book = await response.json();
-
-        if (book) {
-            document.getElementById('pp-book-img').src = book.cover_image.url;
-            document.getElementById('pp-book-img').alt = book.title;
-
-            document.getElementById('pp-title').textContent = book.title;
-            document.getElementById('pp-author').textContent = book.author;
-            document.getElementById('pp-isbn').textContent = `ISBN: ${book.isbn}`;
-            document.getElementById('pp-category').textContent = `Kategori: ${book.category}`;
-
-            document.getElementById('pp-summary').textContent = `Sammanfattning: ${book.description}`;
-
-            document.getElementById('pp-price').innerHTML = `${book.price} SEK`;
+        const response = await fetch(`https://bookshop-backend-phi.vercel.app/products/`); 
+        const books = await response.json();
+        if (books) {
+            return books;
         } else {
             console.error('No books recieved from API');
         }
     } catch (error) {
         console.error('Error fetching book:', error);
     }
-    console.log();
 }
 
-fetchBookData();
+// Find and display book info from id
+async function displayBookData(id) {
+    const data = await fetchBookData();
+    const book = data.find(books => books._id === id);
+    
+    if (book) {
+        console.log(`Title: ${book.title}`);
+        document.getElementById('pp-book-img').src = book.cover_image.url;
+        document.getElementById('pp-book-img').alt = book.title;
+
+        document.getElementById('pp-title').textContent = book.title;
+        document.getElementById('pp-author').textContent = book.author;
+        document.getElementById('pp-isbn').textContent = `ISBN: ${book.isbn}`;
+        document.getElementById('pp-category').textContent = `Kategori: ${book.category}`;
+
+        document.getElementById('pp-summary').textContent = `Sammanfattning: ${book.description}`;
+
+        document.getElementById('pp-price').innerHTML = `${book.price} SEK`;
+    } else {
+        console.log(`Book not found`);
+        }
+};
+
+// Display book info on page (with example id for now)
+displayBookData("6756f2bf23581d968ded9e4c");
+
